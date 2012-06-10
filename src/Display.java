cb69d00e7c8dcf35e978ba92c8b2927483673350
charmjunewonder <charmjunewonder@gmail.com>
2012-06-10 17:48:00 +0800

    Patient

---
+++ src/Display.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+/**
+ * @author charmjunewonder
+ * 
+ */
+public interface Display {
+
+	/**
+	 * @param hearRete
+	 * @param warning
+	 */
+	public void displayHeartRate(int hearRete, boolean warning);
+
+	/**
+	 * @param temperature
+	 * @param warning
+	 */
+	public void displayTemperature(int temperature, boolean warning);
+
+	/**
+	 * @param pressure
+	 * @param warning
+	 */
+	public void displayBloodPressure(int pressure, boolean warning);
+
+	/**
+	 * @param bloodGlucoseLevel
+	 * @param warning
+	 */
+	public void displayBloodGlucoseLevel(int bloodGlucoseLevel, boolean warning);
+
+	/**
+	 * @param stateInfo
+	 */
+	public void displayInfo(String stateInfo);
+}
---
+++ src/Monitor.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+
+/**
+ * @author charmjunewonder
+ * 
+ */
+public class Monitor {
+
+	private Patient currentPatient;
+
+	/**
+	 * @param patient
+	 */
+	public Monitor(Patient patient) {
+		currentPatient = patient;
+	}
+
+	/**
+	 * 
+	 */
+	public void start() {
+	}
+
+	public void processHeartRate(int rate) {
+	}
+
+	public void processBloodPressure() {
+	}
+
+	public void processBloodGlucoseLevel() {
+	}
+
+	public void processTemperature() {
+	}
+}
